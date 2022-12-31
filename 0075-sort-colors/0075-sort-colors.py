659fad0d146d4754d555af7d763774174a6c19a6
albetre21 <121690815+albetre21@users.noreply.github.com>
2022-12-31 17:42:03 +0300

    Time: 30 ms (96.16%), Space: 13.8 MB (62.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = cur = 0
+        right = len(nums)-1
+        while cur<=right:
+            if nums[cur]==0:
+                nums[left],nums[cur]=nums[cur],nums[left]
+                left+=1
+                cur+=1
+            elif nums[cur]==2:
+                nums[cur],nums[right]=nums[right],nums[cur]
+                right-=1
+            else:
+                cur+=1
+                
+                
+                
+        
