9f72aea68a9ae1692695c8caa703fa54b4681f44
albetre21 <121690815+albetre21@users.noreply.github.com>
2023-01-02 16:25:20 +0300

    Time: 1454 ms (88.10%), Space: 29.7 MB (95.03%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        if len(changed) % 2 == 1:
+            return []
+
+        changed.sort()
+        arr1 = []
+        len_arr2 = 0
+
+        match_idx = 0
+        for idx, value in enumerate(changed):       
+            if len(arr1) == len_arr2:
+                arr1.append(value)
+                continue
+            if value < arr1[match_idx] * 2:
+                arr1.append(value)
+            elif value == arr1[match_idx] * 2:
+                len_arr2 += 1
+                match_idx += 1
+            else:
+                return []
+
+        return arr1 if len(arr1) == len_arr2 else []
