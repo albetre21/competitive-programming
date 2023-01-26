ddadfe2eac890f37baf6135cd838e5f134e7817a
albetre21 <121690815+albetre21@users.noreply.github.com>
2023-01-26 11:13:31 +0300

    Time: 88 ms (20.66%), Space: 15.1 MB (36.68%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,13 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        ret = []
-        for i in range(1, n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                ret.append('FizzBuzz')
-            elif i % 3 == 0:
-                ret.append('Fizz')
-            elif i % 5 == 0:
-                ret.append('Buzz')
+        def fb(i):
+            if (i%3==0) and  (i%5==0):
+                return "FizzBuzz"
+            elif i%3==0:
+                return "Fizz"
+            elif i%5==0:
+                return "Buzz"
             else:
-                ret.append(str(i))
-        return ret
+                return str(i)
+        return[fb(i) for i in range (1,n+1)]
+        
+     
