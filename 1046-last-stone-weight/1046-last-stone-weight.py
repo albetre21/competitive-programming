cb4daf5f29ebf6e3181a3b6655eefe668aefbd7a
albetre21 <121690815+albetre21@users.noreply.github.com>
2023-02-04 22:25:51 +0300

    Time: 38 ms (46.15%), Space: 13.9 MB (55.26%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -4,10 +4,10 @@ class Solution:
         for stone in stones:
             heapq.heappush(heap,-stone)
         while len(heap)>=2:
-            x1 = heapq.heappop(heap)
-            x2 = heapq.heappop(heap)
-            if x1 != x2:
-                heapq.heappush(heap,(x1-x2))
+            x = heapq.heappop(heap)
+            y = heapq.heappop(heap)
+            if x != y:
+                heapq.heappush(heap,(x-y))
         return -heap[-1] if heap else 0
         
    
